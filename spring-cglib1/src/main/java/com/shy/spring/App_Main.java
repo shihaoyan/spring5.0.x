416f80f9839d4632748b0ba5fb76aefc4d938095
shihaoyan <923463567@qq.com>
2020-04-20 19:30:24 +0800

    spring5源码分析源码+注释解析

--- spring-cglib1/src/main/java/com/shy/spring/App_Main.java
+++ spring-cglib1/src/main/java/com/shy/spring/App_Main.java
@@ -18,7 +18,10 @@ public class App_Main {
 		 * 基于注解的方式初始化context(环境、上下文、容器)
 		 */
 		AnnotationConfigApplicationContext acac = new AnnotationConfigApplicationContext(AppConfig.class);
+		//看这里想到了一点东西，它里面并没有直接进行获取对象，而是先封装了一个类行
+		//NamedBeanHolder
 		CityService cityService = acac.getBean(CityService.class);
+
 		cityService.queryCity();
 		System.out.println(cityService instanceof CityService);
 		//测试github
